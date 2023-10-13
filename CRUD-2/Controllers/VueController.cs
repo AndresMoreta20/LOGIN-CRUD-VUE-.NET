924e3cf33b783c9e51e9bede9aaae56b6530fdba
AndresMoreta20 <andresmoreta2020@gmail.com>
2023-10-12 20:17:21 -0500

    configuracion

---
+++ CRUD-2/Controllers/VueController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD_2.Controllers
+{
+    public class VueController : Controller
+    {
+        public IActionResult Index()
+        {
+            return File("~/dist/index.html", "text/html");
+        }
+    }
+}
